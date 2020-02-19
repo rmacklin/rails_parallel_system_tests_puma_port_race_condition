34960c787ca3525883289da676cb2bf328b1e0fd
Richard Macklin <richard.github@nrm.com>
2020-02-19 14:06:36 -0800

    Print the port of each parallel worker's Capybara app server

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,11 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+
+  parallelize_setup do |worker_number|
+    driver.use
+
+    puma_port = Capybara.current_session.server.port
+    puts "Worker #{worker_number}'s puma port: #{puma_port}"
+  end
 end
